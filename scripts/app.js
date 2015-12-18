52d5692c934125ba021796aaf92798e2160bc4b5
Lucas Bonomi <lucas.bonomi@gmail.com>
2015-12-18 16:58:43 +0100

    Few responsive goodness

--- scripts/app.js
+++ scripts/app.js
@@ -18,20 +18,6 @@ var $ = function(element) {
   return document.querySelector(element) || document.querySelectorAll(element);
 }
 
-    // var clone = $('#header').innerHTML;
-    // $('nav header').innerHTML = clone;
-
-
-// var sections = document.querySelectorAll('#documentation > section');
-// var cl_zone = document.querySelectorAll('#documentation > section > header.header');
-// for(i=0, len = sections.length ; i < len; i++) {
-//   var h = sections[i].offsetHeight + 60;
-//   sections[i].classList.add('closed')
-//   sections[i].style.height = h + 'px';
-//   cl_zone[i].addEventListener('click', function(){
-//     this.parentNode.classList.toggle('closed')
-//   })
-// }
 
 function stickyHeader(){
   var headerMin = document.querySelector('#minHeader');
@@ -41,15 +27,9 @@ function stickyHeader(){
   var delta = 360;
   if(fromY >= delta) {
     headerMin.classList.add('appear')
-    // for(i=0, len = navHeaderSpan.length ; i < len; i++) {
-    //   navHeaderSpan[i].style.color='#fff';
-    // }
   }
   if(fromY <= delta) {
     headerMin.classList.remove('appear')
-    // for(i=0, len = navHeaderSpan.length ; i < len; i++) {
-    //   navHeaderSpan[i].style.color='';
-    // }
   }
 }
 
@@ -59,7 +39,15 @@ window.onscroll = function(e) {
 
 window.onload = function(e) {  
  stickyHeader()
+ isSmall()
 } 
+
+function isSmall(){
+  var w = document.documentElement.clientWidth;
+  if(w<=768){
+    document.querySelector('nav').classList.add('closed')
+  }
+}
 
 document.getElementById('closeNav').addEventListener('click', function(){
   document.querySelector('.control a span.icon').classList.toggle('flip')
--- src/ui.js
+++ src/ui.js
@@ -2,20 +2,6 @@ var $ = function(element) {
   return document.querySelector(element) || document.querySelectorAll(element);
 }
 
-    // var clone = $('#header').innerHTML;
-    // $('nav header').innerHTML = clone;
-
-
-// var sections = document.querySelectorAll('#documentation > section');
-// var cl_zone = document.querySelectorAll('#documentation > section > header.header');
-// for(i=0, len = sections.length ; i < len; i++) {
-//   var h = sections[i].offsetHeight + 60;
-//   sections[i].classList.add('closed')
-//   sections[i].style.height = h + 'px';
-//   cl_zone[i].addEventListener('click', function(){
-//     this.parentNode.classList.toggle('closed')
-//   })
-// }
 
 function stickyHeader(){
   var headerMin = document.querySelector('#minHeader');
@@ -25,15 +11,9 @@ function stickyHeader(){
   var delta = 360;
   if(fromY >= delta) {
     headerMin.classList.add('appear')
-    // for(i=0, len = navHeaderSpan.length ; i < len; i++) {
-    //   navHeaderSpan[i].style.color='#fff';
-    // }
   }
   if(fromY <= delta) {
     headerMin.classList.remove('appear')
-    // for(i=0, len = navHeaderSpan.length ; i < len; i++) {
-    //   navHeaderSpan[i].style.color='';
-    // }
   }
 }
 
@@ -43,7 +23,15 @@ window.onscroll = function(e) {
 
 window.onload = function(e) {  
  stickyHeader()
+ isSmall()
 } 
+
+function isSmall(){
+  var w = document.documentElement.clientWidth;
+  if(w<=768){
+    document.querySelector('nav').classList.add('closed')
+  }
+}
 
 document.getElementById('closeNav').addEventListener('click', function(){
   document.querySelector('.control a span.icon').classList.toggle('flip')
